9982f9721a5918dcb6c59f11cfd4ec331b28bd25
salimm <salimm@cs.pitt.edu>
2017-09-06 14:22:40 -0400

    initial commit including old jsonrpc based project

---
+++ me/salimm/jrns/stub/py/JRNSServerStub.py
@@ -0,0 +1,37 @@
+'''
+Created on Jul 21, 2017
+
+@author: Salim
+'''
+from me.salimm.jrns.stub.py.JRNSStub import JRNSStub
+
+
+class JRNSServerStub(JRNSStub):
+    
+    
+    def __init__(self, port):
+        self.port = port
+        self.provider = None
+        
+        
+    def init(self, provider):
+        pass
+    
+    
+    def start(self):
+        pass
+    
+    
+    
+    def register(self, nsInfo):
+        pass
+        
+    
+    
+    def getService(self):
+        pass
+    
+    
+    def getProvider(self):
+        pass
+        
---
+++ me/salimm/jrns/stub/py/JRNSStub.py
@@ -0,0 +1,49 @@
+'''
+Created on Jul 21, 2017
+
+@author: Salim
+'''
+import json
+import requests
+
+from  me.salimm.jrns.common.Constants import RPC_PROTOCOL
+from me.salimm.jrns.common.info.ServiceInfo import ServiceInfo
+
+
+class JRNSStub:
+    
+    def __init__(self):
+        pass
+    
+    
+    def getServiceInfoById(self, ns_info, service_id):        
+        url = RPC_PROTOCOL + ns_info.getAddress() + ":" + str(ns_info.getPort()) + "/NSService.json"
+        headers = {'content-type': 'application/json'}
+    
+        # Example echo method
+        payload = {
+            "method": "getServiceInfoById",
+            "params": [service_id],
+            "jsonrpc": "2.0",
+            "id": 0,
+        }
+        response = requests.post(
+            url, data=json.dumps(payload), headers=headers).json()
+    
+        return ServiceInfo.from_dict(response['result']);
+    
+    def getServiceInfoByName(self, ns_info, service_name):
+        url = RPC_PROTOCOL + ns_info.getAddress() + ":" + str(ns_info.getPort()) + "/NSService.json"
+        headers = {'content-type': 'application/json'}
+    
+        # Example echo method
+        payload = {
+            "method": "getServiceInfoByName",
+            "params": [service_name],
+            "jsonrpc": "2.0",
+            "id": 0,
+        }
+        response = requests.post(
+            url, data=json.dumps(payload), headers=headers).json()
+    
+        return ServiceInfo.from_dict(response['result'])
