3ac935fa302ae8fa85930f2e4d7c0b36699044b7
salimm <salimm@cs.pitt.edu>
2018-01-22 03:45:54 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   license = 'MIT',
   url = 'https://github.com/LessRPC/lessrpc_stub_py2x', # use the URL to the github repo
   download_url = 'https://github.com/LessRPC/lessrpc_stub_py2x/archive/1.0.1.tar.gz', # I'll explain this in a second
-  keywords = ['python','serialization','deserialization','rpc','rmi','less rpc', 'clinet', 'server'], # arbitrary keywords
+  keywords = ['python','serialization','deserialization','rpc','rmi','less rpc', 'client', 'server'], # arbitrary keywords
   classifiers = ['Programming Language :: Python :: 2.7'],
   install_requires=[],
   python_requires='~=2.7',
