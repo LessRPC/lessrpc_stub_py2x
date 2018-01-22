71e1ba31e62129123c0fff136dd713c09fb2cfed
salimm <salimm@cs.pitt.edu>
2018-01-22 03:48:54 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'lessrpc-stub',
   packages = ['lessrpc'], # this must be the same as the name above
-  version = '1.0.1',
+  version = '1.0.2',
   description = 'Less RPC stubs project',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/LessRPC/lessrpc_stub_py2x', # use the URL to the github repo
-  download_url = 'https://github.com/LessRPC/lessrpc_stub_py2x/archive/1.0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/LessRPC/lessrpc_stub_py2x/archive/1.0.2.tar.gz', # I'll explain this in a second
   keywords = ['python','serialization','deserialization','rpc','rmi','less rpc', 'client', 'server'], # arbitrary keywords
   classifiers = ['Programming Language :: Python :: 2.7'],
   install_requires=[],
