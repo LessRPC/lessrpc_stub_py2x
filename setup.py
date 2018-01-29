22b324c1a46467c9bb495c13dfe67785d0e1389c
salimm <salimm@cs.pitt.edu>
2018-01-29 00:22:22 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'lessrpc_stub',
   packages = ['lessrpc_stub','lessrpc_stub.stubs'], # this must be the same as the name above
-  version = '1.0.6',
+  version = '1.0.7',
   description = 'Less RPC stubs project',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/LessRPC/lessrpc_stub_py2x/', # use the URL to the github repo
-  download_url = 'https://github.com/LessRPC/lessrpc_stub_py2x/archive/1.0.6.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/LessRPC/lessrpc_stub_py2x/archive/1.0.7.tar.gz', # I'll explain this in a second
   keywords = ['python','serialization','deserialization','rpc','rmi','less rpc', 'client', 'server'], # arbitrary keywords
   classifiers = ['Programming Language :: Python :: 2.7'],
   install_requires=['pylods','pylods-json','httpoutputsteam','cherrypy','enum34' ],
